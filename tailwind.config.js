da1b02ad8727edc27822c19b93f892f580db2e0f
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-05-05 11:30:49 +0900

    chore: configure colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Space Mono', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        blue: 'hsl(212, 100%, 50%)',
+        gray: 'hsl(217, 20%, 51%)',
+        grayishBlue: 'hsl(217, 35%, 45%)',
+        black: 'hsl(217, 21%, 21%)',
+        lightBlue: 'hsl(227, 100%, 98%)',
+        darkGray: 'hsl(222, 41%, 20%)',
+      },
+    },
   },
   plugins: [],
 };
