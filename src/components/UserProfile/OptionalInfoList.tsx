6e364af4fc6bd2991ca0d7923507d61baebb078c
Yuki Yamamura <yuki.yamamura@icloud.com>
2023-05-21 06:53:34 +0900

    fix: prevent overflowing grid items

--- src/components/UserProfile/OptionalInfoList.tsx
+++ src/components/UserProfile/OptionalInfoList.tsx
@@ -9,7 +9,7 @@ type Props = {
 };
 
 const OptionalInfoList: FC<Props> = ({ user }) => (
-  <ul className="grid gap-y-4 xs:grid-flow-col xs:grid-rows-2 xs:gap-y-4">
+  <ul className="grid gap-y-4 xs:grid-flow-col xs:grid-cols-2 xs:grid-rows-2 xs:gap-4">
     {infoList.map((info) => (
       <OptionalInfo user={user} info={info} key={info} />
     ))}
